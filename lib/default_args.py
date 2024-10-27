86ead700f0a27cd16cc5a9f70b12fb1c3d6ad191
IamHussain <33412791+IamHussain503@users.noreply.github.com>
2024-10-27 18:09:24 +0500

    Update default_args.py

--- lib/default_args.py
+++ lib/default_args.py
@@ -3,6 +3,6 @@ default_args = {
     'subtensor_network': 'finney',
     'netuid': 50,
     'wallet_name': 'cold',
-    'wallet_hotkey': 'hot'
+    'wallet_hotkey': 'hot',
     'process_name': 'validator',
 }
