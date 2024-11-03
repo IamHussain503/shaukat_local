da39e83ebe2a678293e79b42ea9e2ac8a22703aa
IamHussain <33412791+IamHussain503@users.noreply.github.com>
2024-11-03 23:08:33 +0500

    updated port

--- neurons/validator.py
+++ neurons/validator.py
@@ -33,10 +33,10 @@ class AIModelController():
 
     async def run_fastapi_with_ngrok(self, app):
         # Setup ngrok tunnel
-        ngrok_tunnel = ngrok.connect(40191, bind_tls=True)
+        ngrok_tunnel = ngrok.connect(40180, bind_tls=True)
         print('Public URL:', ngrok_tunnel.public_url)
         # Create and start the uvicorn server as a background task
-        config = uvicorn.Config(app=app, host="0.0.0.0", port=40191)  # Ensure port matches ngrok's
+        config = uvicorn.Config(app=app, host="0.0.0.0", port=40180)  # Ensure port matches ngrok's
         server = uvicorn.Server(config)
         # No need to await here, as we want this to run in the background
         task = asyncio.create_task(server.serve())
