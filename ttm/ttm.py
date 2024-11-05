845e822e1d8e0426c5dde80429fef1e768f59dd5
IamHussain <33412791+IamHussain503@users.noreply.github.com>
2024-11-05 11:47:40 +0500

    Update ttm.py

--- ttm/ttm.py
+++ ttm/ttm.py
@@ -405,12 +405,12 @@ class MusicGenerationService(AIModelService):
         if self.combinations:
             current_combination = self.combinations.pop(0)
             bt.logging.info(f"Current Combination for TTM: [68,73]")
-            filtered_axons = [self.metagraph.axons[i] for i in [72,73]]
+            filtered_axons = [self.metagraph.axons[i] for i in [79,73]]
         else:
             self.get_filtered_axons()
             current_combination = self.combinations.pop(0)
             bt.logging.info(f"Current Combination for TTM: [68,73]")
-            filtered_axons = [self.metagraph.axons[i] for i in [72,73]]
+            filtered_axons = [self.metagraph.axons[i] for i in [79,73]]
 
         return filtered_axons
     
